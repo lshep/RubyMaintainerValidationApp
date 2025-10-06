0e0e1b71e39e202657b1adb7eb483e71b99d6e4d
lshep <lori.shepherd@roswellpark.org>
2025-10-06 10:18:24 -0400

    Preserve existing files: update submodule content

--- runApp.sh
+++
@@ -1,6 +0,0 @@
-# change to true if want to send real aws ses emails 
-export SEND_VERIFICATION_EMAILS=false
-bundle exec ruby app.rb
-
-## by default Sinatra listens on localhost:4567
-## test endpoints on http://localhost:4567
